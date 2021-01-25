7f61c2d68946c3751778f5eecdd8608d03fb7966
Adam Mańk <adammank.it@gmail.com>
2021-01-26 00:07:41 +0100

    Fixed comments

--- app/car/service.py
+++ app/car/service.py
@@ -13,6 +13,7 @@ class CarService:
     def check_if_the_model_exists_in_the_external_api(self):
         car_list = self._get_car_list()
 
+        # Each car in the car list is a separate dictionary.
         for car_model in car_list:
             car_model_name = car_model.get('Model_Name')
             if self.car_model_name == car_model_name:
@@ -24,5 +25,5 @@ class CarService:
             url=constants.external_car_api_url(self.car_make_name)
         ).json()
 
-        # Returned dict has car list as a value for the key "Results"
+        # Returned dict has car list as a value for the key "Results".
         return car_list.get('Results')
