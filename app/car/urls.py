aabd8fa48a3ed48f6d0b7f52be65ea417f03b6b5
Adam Mańk <adammank.it@gmail.com>
2021-03-30 12:07:01 +0200

    Register cars ulr in router

--- app/car/urls.py
+++ app/car/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 
 router = DefaultRouter()
+router.register(r'cars', views.CarModelViewSet, basename='cars')
 router.register(r'rates', views.CarModelRateViewSet, basename='rates')
 
 urlpatterns = [
