66563fdf12fd3a0741449148eb03116dd8b5121b
Adam Mańk <adammank.it@gmail.com>
2021-03-30 11:59:39 +0200

    Optimize CarModel view

--- app/car/views.py
+++ app/car/views.py
@@ -1,50 +1,41 @@
-from rest_framework import generics
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
+from django.db import transaction
 from django.db.models import Count
 
-from .models import CarMake, CarModel, CarModelRate
+from .models import CarModel, CarModelRate
 from .serializers import CarModelSerializer, CarModelRateSerializer
 from .service import CarService
+from .utils import create_car_make_instance
+from .constants import CAR_NOT_FOUND
 
 
-class CarListCreateAPIView(generics.ListCreateAPIView):
+class CarModelViewSet(viewsets.ModelViewSet):
     serializer_class = CarModelSerializer
     queryset = CarModel.objects.all()
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
-        """Based on the retrived values, checks if the car exists in the external API.
-        If yes - creates two instances: CarMake & CarModel,
+        """Based on the retrieved values, checks if the car exists in the external API.
+        It yes - creates two instances: CarMake & CarModel,
                  responds with 201 Created status.
         If no - responds with 404 Not Found status."""
 
-        make_name = request.data.get('make')
-        model_name = request.data.get('model_name')
+        car_make_name = request.data.get('make')
+        car_model_name = request.data.get('model')
+
         car_service = CarService(
-            car_make_name=make_name, car_model_name=model_name)
+            car_make_name=car_make_name,
+            car_model_name=car_model_name
+        )
 
         if car_service.model_exists():
-
-            car_make_instance, created = CarMake.objects.get_or_create(
-                make_name=make_name)
-
-            data = {
-                'make': car_make_instance.make_name,
-                'model_name': model_name
-            }
-            car_model_serializer = super().get_serializer(data=data)
-
-            if car_model_serializer.is_valid():
-                car_model_serializer.save()
-                return Response(car_model_serializer.data,
-                                status=status.HTTP_201_CREATED)
-            else:
-                return Response(car_model_serializer.errors,
-                                status=status.HTTP_400_BAD_REQUEST)
-
+            create_car_make_instance(car_make_name)
+            return super().create(request, *args, **kwargs)
         else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
+            return Response(
+                data=CAR_NOT_FOUND,
+                status=status.HTTP_404_NOT_FOUND)
 
 class CarPopularListAPIView(generics.ListAPIView):
     serializer_class = CarModelSerializer
